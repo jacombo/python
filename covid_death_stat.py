615cffdedb37165b226d097c22a32638b75b8b5a
jacombo <zdenek.jakubec@gmail.com>
2020-10-28 11:54:30 +0100

    percentage counting added

--- covid_death_stat.py
+++ covid_death_stat.py
@@ -6,34 +6,34 @@ import operator
 
 response = requests.get('https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/umrti.json')
 
-print("*****************************************************************************")
-print("Datum:%s\tzdroj:%s" % (response.json()['modified'],response.json()['source']))
-print("*****************************************************************************")
+print("***************************************************************")
+print("Aktualizovano:%s\nZdroj:%s" % (response.json()['modified'],response.json()['source']))
+print("***************************************************************")
 
-sum = {}
+summary = {}
 age_count = 0
+total_deaths = 0
 for item in response.json()['data']:
     #print("%s %s" % (item['vek'], round(item['vek'],-1)))
     age = round(item['vek'],-1)
     age_count = age_count + age
 
-    if age in sum:
-        count = sum[age]
+    if age in summary:
+        count = summary[age]
     else:
         count = 1    
     
-    sum[age] = count + 1
+    summary[age] = count + 1
+    total_deaths = total_deaths + 1
 
 # Sort Dictionary by value using itemgetter
-sorted_dict = dict(sorted(sum.items(),
+sorted_dict = dict(sorted(summary.items(),
                             key=operator.itemgetter(1),
                             reverse=True))
-total = 0
+
 for k,v in sorted_dict.items():
-    print("Vek:%s\tpocet umrti:%s" % (k,v))
-    total = total + v
+    print("Vek:%s+\t\tpocet umrti:%s\t\t%s%%" % (k,v, round((v / total_deaths) * 100, 1)))
 
-print("\nPrumerny vek mrtvych:%s" % (age_count / total))
-print("\nCelkovy pocet mrtvych:%s" % (total))
-print("*****************************************************************************")
-
+print("\nPrumerny vek mrtvych:%s" % (round(age_count / total_deaths, 1)))
+print("\nCelkovy pocet mrtvych:%s" % (total_deaths))
+print("***************************************************************")
